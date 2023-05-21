0fba75f7592c90d8f0fa09682c09084e2a842ef4
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-05-22 01:39:06 +0200

    +1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { useEffect, lazy } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
-// import { PrivateRoute } from './PrivateRoute';
-// import { RestrictedRoute } from './RestrictedRoute';
-// import { refreshUser } from 'redux/auth/operations';
-// import { useAuth } from 'hooks';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
+import { refreshUser } from '../redux/auth/operations';
+import { useAuth } from '../hooks/userAuth';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
@@ -13,13 +13,39 @@ const LoginPage = lazy(() => import('../pages/Login'));
 const ContactsPage = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth;
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    'Refreshing page...'
+  ) : (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/contacts" element={<ContactsPage />}></Route>
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute
+              redirectTo="/contacts"
+              component={<RegisterPage />}
+            />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/contacts" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
       </Route>
       {/* <Route path="*" element={<PageNotFound />} /> */}
     </Routes>
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,6 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { register, logIn, logOut, refreshUser } from './operations';
-import Login from 'pages/Login';
 
 const initialState = {
   user: { name: null, email: null },
@@ -31,36 +30,11 @@ const authSlice = createSlice({
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
       }),
-
-  // extraReducers: {
-  //   [register.fulfilled](state, action) {
-  //     state.user = action.payload.user;
-  //     state.token = action.payload.token;
-  //     state.isLoggedIn = true;
-  //   },
-  //   [logIn.fulfilled](state, action) {
-  //     state.user = action.payload.user;
-  //     state.token = action.payload.token;
-  //     state.isLoggedIn = true;
-  //   },
-  //   [logOut.fulfilled](state) {
-  //     state.user = { name: null, email: null };
-  //     state.token = null;
-  //     state.isLoggedIn = false;
-  //   },
-  //   [refreshUser.pending](state) {
-  //     state.isRefreshing = true;
-  //   },
-  //   [refreshUser.fulfilled](state, action) {
-  //     state.user = action.payload;
-  //     state.isLoggedIn = true;
-  //     state.isRefreshing = false;
-  //   },
-  //   [refreshUser.rejected](state) {
-  //     state.isRefreshing = false;
-  //   },
-  // },
 });
 
 export const authReducer = authSlice.reducer;
