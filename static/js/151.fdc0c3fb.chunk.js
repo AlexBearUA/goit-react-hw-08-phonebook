cf1b4792303d78a264be476da52f3c4e366cb544
AlexBearUA <113371691+AlexBearUA@users.noreply.github.com>
2023-05-27 17:13:58 +0000

    Deploying to gh-pages from @ AlexBearUA/goit-react-hw-08-phonebook@17ee7985dc717a1e60c6c106fad3449556973728 🚀

---
+++ static/js/151.fdc0c3fb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[151],{3151:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var c="Home_container__AHomM",a="Home_bgImg__l3kZO",r=t(184),o=function(){return(0,r.jsxs)("div",{className:c,children:[(0,r.jsx)("h1",{children:"All your contacts in one place"}),(0,r.jsx)("div",{className:a})]})}}}]);
+//# sourceMappingURL=151.fdc0c3fb.chunk.js.map
--- static/js/266.54f45a25.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,o){o.r(t);var r=o(184);t.default=function(){return(0,r.jsx)("div",{children:"Wellcome to your phonebook"})}}}]);
-//# sourceMappingURL=266.54f45a25.chunk.js.map
